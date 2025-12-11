d1fcdeb0ed3844ef3d40b476a41e381de2a0e9d9
Aino Spring <info@aino-spring.com>
2025-12-11 13:50:43 +0100

    Flake

--- hosts/labeltc/configuration.nix
+++ hosts/labeltc/configuration.nix
@@ -1,14 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
