95a05bba538f76589c91549f9abe989fdbff5518
Aino Spring <info@aino-spring.com>
2025-12-11 18:46:19 +0100

    Label printserver

--- modules/label.nix
+++ modules/label.nix
@@ -1,13 +1,31 @@
 { config, lib, system, label, ... }:
 
+let
+	labelpkgs = label.packages.${system};
+in
 {
 	options.label = {
 		enable = lib.mkEnableOption "Enable the label module";
+		printserver.port = lib.mkOption {
+			type = lib.types.string;
+			default = "8080";
+			description = "Label printing server port";
+		};
 	};
 
 	config = lib.mkIf config.label.enable {
 		environment.systemPackages = [
-			label.packages.${system}.textlabel
+			labelpkgs.textlabel
 		];
+
+		systemd.services.printserver = {
+			enable = true;
+			description = "Label printing server";
+			serviceConfig = {
+				ExecStart = "${labelpkgs.printserver}/bin/printserver zebra ${config.printserver.port}";
+				Restart = "always";
+			};
+			wantedBy = [ "multi-user.target" ];
+		};
 	};
 }
